efc6cb186ecb8255cbb9b3ddf898eedc109d46df
wykkyw <1309562521@qq.com>
2017-09-17 21:45:01 +0800

    Add files via upload

---
+++ 作业01/js/js.js
@@ -0,0 +1,13 @@
+var a1=document.getElementById('a1');
+var a2=document.getElementById('a2');
+var a2=document.getElementById('a3');
+var lk=document.getElementById('link');
+			a1.onclick=function(){
+				lk.href="css/css1.css";
+			}
+			a2.onclick=function(){
+				lk.href="css/css2.css";
+			}
+			a3.onclick=function(){
+				lk.href="css/css3.css";
+			}
